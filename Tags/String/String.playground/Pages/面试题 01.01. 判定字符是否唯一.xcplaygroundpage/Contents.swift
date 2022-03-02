b51af7361020d5ffe63a9425f8ea11cc9463d86d
CaoSuyang <caosuyang1026@gmail.com>
2022-03-02 17:29:34 +0800

    面试题 01.01. 判定字符是否唯一

--- Tags/String/String.playground/Pages/面试题 01.01. 判定字符是否唯一.xcplaygroundpage/Contents.swift
+++ Tags/String/String.playground/Pages/面试题 01.01. 判定字符是否唯一.xcplaygroundpage/Contents.swift
@@ -17,11 +17,21 @@ var greeting = "Hello, playground"
 // Mark - Solution
 /**
  * A.暴力法
+ * 遍历字符串，每次遍历去除首字母，然后查询剩余字符串里是否有去除掉的这个首字母
  * @param astr
  * @return
  */
 func isUniqueA(_ astr: String) -> Bool {
-    
+    if astr.count > 128 {
+        return false
+    }
+    var s = astr
+    for subS in s {
+        s.removeFirst()
+        if s.contains(subS) {
+            return false
+        }
+    }
     return true
 }
 
@@ -31,18 +41,32 @@ func isUniqueA(_ astr: String) -> Bool {
  * @return
  */
 func isUniqueB(_ astr: String) -> Bool {
-    
+    if astr.count > 128 {
+        return false
+    }
+    let s = astr.sorted()
+    var pre = ""
+    for subS in s {
+        if String(subS) == pre {
+            return false
+        }
+        pre = String(subS)
+    }
     return true
 }
 
 /**
  * C.Set去重
+ * 利用Set是无序无重复数据的集和，去掉String中重复的元素。与原字符串长度对比。
  * @param astr
  * @return
  */
 func isUniqueC(_ astr: String) -> Bool {
-    
-    return true
+    if astr.count > 128 {
+        return false
+    }
+    let s = Set(astr)
+    return s.count == astr.count
 }
 
 /**
@@ -51,7 +75,17 @@ func isUniqueC(_ astr: String) -> Bool {
  * @return
  */
 func isUniqueD(_ astr: String) -> Bool {
-    
+    if astr.count > 128 {
+        return false
+    }
+    var map: [Character: Int] = [:]
+    let strs = Array(astr)
+    for (index, value) in strs.enumerated() {
+        if (map[value] != nil) {
+            return false
+        }
+        map[value] = index
+    }
     return true
 }
 
@@ -61,7 +95,18 @@ func isUniqueD(_ astr: String) -> Bool {
  * @return
  */
 func isUniqueE(_ astr: String) -> Bool {
-    
+    if astr.count > 26 {
+        return false
+    }
+    var checker = 0
+    for char in astr {
+        let a: String.Element = "a"
+        let moveBit = (char.asciiValue ?? 0) - (a.asciiValue ?? 0)
+        if (checker & (1 << moveBit)) > 0 {
+            return false
+        }
+        checker |= (1 << moveBit)
+    }
     return true
 }
 
