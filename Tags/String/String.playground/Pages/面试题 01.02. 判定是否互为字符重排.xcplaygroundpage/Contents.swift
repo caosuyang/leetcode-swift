d861c8f740ec6989be020fa608bc354250328a8f
CaoSuyang <caosuyang1026@gmail.com>
2022-03-03 18:21:18 +0800

    面试题 01.02. 判定是否互为字符重排

--- Tags/String/String.playground/Pages/面试题 01.02. 判定是否互为字符重排.xcplaygroundpage/Contents.swift
+++ Tags/String/String.playground/Pages/面试题 01.02. 判定是否互为字符重排.xcplaygroundpage/Contents.swift
@@ -7,3 +7,40 @@ var greeting = "Hello, playground"
  * @author csy
  *
  */
+
+
+/**
+ * A.排序
+ * @param s1
+ * @param s2
+ * @return
+ */
+func CheckPermutationA(_ s1: String, _ s2: String) -> Bool {
+    if s1.count != s2.count {
+        return false
+    }
+    return s1.sorted() == s2.sorted()
+}
+
+/**
+ * B.哈希表
+ * @param s1
+ * @param s2
+ * @return
+ */
+func CheckPermutationB(_ s1: String, _ s2: String) -> Bool {
+    if s1.count != s2.count {
+        return false
+    }
+    var chars: [UInt8] = []
+    for char in s1 {
+        chars[char.asciiValue ?? 0] += 1
+    }
+    for char in s2 {
+        chars[char.asciiValue ?? ] -= 1
+        if chars[char.asciiValue ?? 0] < 0 {
+            return false
+        }
+    }
+    return true
+}
