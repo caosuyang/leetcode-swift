a24cec2e880a38fd6e254fe95a44c094573f0382
CaoSuyang <caosuyang1026@gmail.com>
2022-03-04 17:35:19 +0800

    面试题 01.04. 回文排列

--- Tags/String/String.playground/Pages/面试题 01.04. 回文排列.xcplaygroundpage/Contents.swift
+++ Tags/String/String.playground/Pages/面试题 01.04. 回文排列.xcplaygroundpage/Contents.swift
@@ -12,6 +12,40 @@ var greeting = "Hello, playground"
  *
  */
 
+/**
+ * A.哈希表
+ * @param s
+ * @return
+ */
 func canPermutePalindrome(_ s: String) -> Bool {
+    let str = s.uppercased()
+    var map: [Character: Int] = [:]
+    for c in str {
+        if map.keys.contains(c) {
+            var v = map[c] ?? 1
+            v += 1
+            map[c] = v;
+        } else {
+            map[c] = 1;
+        }
+    }
+    print(map)
+    
+    let values = map.values
+    var count = 0
+    for value in values {
+        if value % 2 != 0 {
+            count += 1
+        }
+    }
+    if count > 1 {
+        return false
+    }
     return true
 }
+
+
+// Mark - Test
+//let pali = "Rats live on no evil star"
+let pali = "AaBb//a"
+print(canPermutePalindrome(pali))
