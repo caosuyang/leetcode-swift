dd646303d18c1ad9fb71972c370a44247d28983c
caosuyang <caosuyang1026@gmail.com>
2023-06-27 15:18:16 +0800

    top-array-121. 买卖股票的最佳时机 top-array-88. 合并两个有序数组

--- leetcode/Top/Array/121. 买卖股票的最佳时机 .playground/Contents.swift
+++ leetcode/Top/Array/121. 买卖股票的最佳时机 .playground/Contents.swift
@@ -1,3 +1,81 @@
-import UIKit
+/**
+ * 121. 买卖股票的最佳时机
+ * https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/description/
+ * @author csy
+ */
 
-var greeting = "Hello, playground"
+/**
+ * 方法一：暴力法
+ * 时间复杂度：O(n^2)，n为数组长度，2层遍历for循环
+ * 空间复杂度：O(1)，需要常数空间存放变量
+ */
+func maxProfit(_ prices: [Int]) -> Int {
+    let len = prices.count
+    var max = 0
+    for i in 0..<len {
+        for j in i+1..<len {
+            let newMax = prices[j] - prices[i]
+            if newMax > max {
+                max = newMax
+            }
+        }
+    }
+    return max
+}
+
+/**
+ * 方法二：一次遍历
+ * 时间复杂度：O(n)，n为数组长度，一次遍历记录历史最低价格
+ * 空间复杂度：O(1)，需要常数空间存放变量
+ */
+func maxProfit2(_ prices: [Int]) -> Int {
+    var min = Int.max
+    var max = 0
+    let len = prices.count
+    for i in 0..<len {
+        if prices[i] < min { // 记录最低价格
+            min = prices[i]
+        } else if prices[i] - min > max { // 记录差价最大价格
+            max = prices[i] - min
+        }
+    }
+    return max
+}
+
+/**
+ 测试用例
+ */
+let prices1 = [7,1,5,3,6,4]
+let output1 = 5
+let prices2 = [7,6,4,3,1]
+let output2 = 0
+
+/**
+ 执行结果
+ */
+print("maxProfit")
+print(maxProfit(prices1))
+print(maxProfit(prices2))
+assert(maxProfit(prices1) == output1)
+assert(maxProfit(prices2) == output2)
+
+print("maxProfit2")
+print(maxProfit2(prices1))
+print(maxProfit2(prices2))
+assert(maxProfit2(prices1) == output1)
+assert(maxProfit2(prices2) == output2)
+
+/**
+ 控制台打印
+ */
+/**
+ expression failed to parse:
+ error: 121. 买卖股票的最佳时机 .playground:27:1: error: unterminated '/*' comment
+
+ ^
+ */
+
+ 121. 买卖股票的最佳时机 .playground:25:1: note: comment started here
+ /**
+ ^
+  */*/
--- leetcode/Top/Array/88. 合并两个有序数组.playground/Contents.swift
+++ leetcode/Top/Array/88. 合并两个有序数组.playground/Contents.swift
@@ -1,3 +1,113 @@
-import UIKit
+/**
+ * 88. 合并两个有序数组
+ * https://leetcode.cn/problems/merge-sorted-array/description/
+ * @author csy
+ */
 
-var greeting = "Hello, playground"
+/**
+* 方法一：直接合并后排序
+* 时间复杂度：O(nlog(m+n))，sorted排序时间复杂度为log(m+n)
+* 空间复杂度：O(log(m+n))，sorted排序栈空间为log(m+n)
+*/
+func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    for i in 0..<n {
+        nums1[m + i] = nums2[i]
+    }
+    // sort
+    nums1 = nums1.sorted { num1, num2 in
+        num1 < num2
+    }
+}
+
+/**
+* 方法二：双指针
+* 时间复杂度：O(m+n)，n、m为数组长度，1层遍历for循环
+* 空间复杂度：O(m+n))，需要m+n空间存放变量
+*/
+func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    var p1 = 0, p2 = 0
+    var num3 = [Int]()
+    var cur = 0
+
+    while p1 < m || p2 < n {
+        if nums1[p1] < nums2[p2] {
+            num3[cur] = nums1[p1]
+            cur = cur + 1
+            p1 = p1 + 1
+        } else {
+            num3[cur] = nums2[p2]
+            cur = cur + 1
+            p2 = p2 + 1
+        }
+    }
+    nums1 = num3
+}
+
+/**
+* 方法三：双指针
+* 时间复杂度：O(m+n)，n、m为数组长度，1层遍历for循环
+* 空间复杂度：O(m+n))，需要m+n空间存放变量
+*/
+func merge3(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    var p1 = 0, p2 = 0
+    var num3 = [Int]()
+    var cur = 0
+
+    while p1 < m || p2 < n {
+        if p1 == m {
+            cur = nums2[p2]
+            p2 = p2 + 1
+        } else if  p2 == n {
+            cur = nums1[p1]
+            p1 = p1 + 1
+        } else if nums1[p1] < nums2[p2] {
+            cur = nums1[p1]
+            p1 = p1 + 1
+        } else {
+            cur = nums2[p2]
+            p2 = p2 + 1
+        }
+        num3[p1 + p2 - 1] = cur
+    }
+    nums1 = num3
+}
+
+/**
+ 测试用例
+ */
+var nums1 = [1,2,3,0,0,0]
+let m1 = 3, nums2 = [2,5,6], n1 = 3, output1 = [1,2,2,3,5,6]
+var nums3 = [1]
+let m2 = 1, nums4: [Int] = [], n2 = 0, output2 = [1]
+var nums5 = [0]
+let m3 = 0, nums6 = [1], n3 = 1, output3 = [1]
+
+
+/**
+ 执行结果
+ */
+//print("merge1")
+//merge(&nums1, m1, nums2, n1)
+//print(nums1)
+//assert(nums1 == output1)
+//
+//print("merge2")
+//merge2(&nums3, m2, nums4, n2)
+//print(nums3)
+//assert(nums3 == output2)
+
+print("merge3")
+merge3(&nums5, m3, nums6, n3)
+print(nums5)
+assert(nums5 == output1)
+
+/**
+ 控制台打印
+ */
+
+/**
+ 执行结果
+ */
+/**
+ process exited with signal SIGILL
+ */
